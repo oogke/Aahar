59eaa5570669dbd701d4b5c181351d6482891a0a
oogke <actlikeanut@gmail.com>
2025-05-22 14:32:27 +0545

    may 22

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose= require("mongoose");
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminRouter=require('./routes/admin');
 
 var app = express();
+
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('admin/admin.ejs', { title: 'Express' });
+  res.render('admin/admin.ejs');
+});
+
+router.get('/product', function(req, res, next) {
+  res.render('admin/product.ejs');
+});
+
+router.get('/contactDetail', function(req, res, next) {
+  res.render('admin/contactDetails.ejs');
 });
 
 module.exports = router;
